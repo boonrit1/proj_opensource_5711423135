55045fec1d9ba5e08007781e5d1b182942bcfd49
boonrit1 <boonrit.tasi57@sskru.ac.th>
2016-09-09 14:16:50 +0700

    boonrit

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("boonrit 5711423135");
+            // keep the console window open in debug mode.
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
